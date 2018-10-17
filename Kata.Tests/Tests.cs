aedfcb2c997ebc7650c3861f316deac177fd94f3
Paul Phillips <psphilli@gmail.com>
2018-10-17 17:15:08 -0400

    kata's performed to reach 6kyu

--- Kata.Tests/Tests.cs
+++ Kata.Tests/Tests.cs
@@ -5,83 +5,137 @@ using System.Collections.Generic;
 namespace Codewars
 {
     [TestFixture]
-    public class UpdateLight
-    {
-        [TestCase("green", "yellow")]
-        [TestCase("yellow", "red")]
-        [TestCase("red", "green")]
-        public void BasicTests(string s, string expected)
+    public class ValidatePinTests
+    {   
+        [Test, Description("ValidatePin should return false for pins with length other than 4 or 6")]
+        public void LengthTest()
         {
-            Assert.That(Kata.UpdateLight(s), Is.EqualTo(expected));
+            Assert.AreEqual(false, Kata.ValidatePin("1"), "Wrong output for \"1\"");
+            Assert.AreEqual(false, Kata.ValidatePin("12"), "Wrong output for \"12\"");
+            Assert.AreEqual(false, Kata.ValidatePin("123"), "Wrong output for \"123\"");
+            Assert.AreEqual(false, Kata.ValidatePin("12345"), "Wrong output for \"12345\"");
+            Assert.AreEqual(false, Kata.ValidatePin("1234567"), "Wrong output for \"1234567\"");
+            Assert.AreEqual(false, Kata.ValidatePin("-1234"), "Wrong output for \"-1234\"");
+            Assert.AreEqual(false, Kata.ValidatePin("1.234"), "Wrong output for \"1.234\"");
+            Assert.AreEqual(false, Kata.ValidatePin("-1.234"), "Wrong output for \"-1.234\"");
+            Assert.AreEqual(false, Kata.ValidatePin("00000000"), "Wrong output for \"00000000\"");
         }
-        [Test]
-        public void RandomTests()
+        
+        [Test, Description("ValidatePin should return false for pins which contain characters other than digits")]
+        public void NonDigitTest()
         {
-            var rnd = new Random();
-            for (int i = 0; i < 10; i++)
+            Assert.AreEqual(false, Kata.ValidatePin("a234"), "Wrong output for \"a234\"");
+            Assert.AreEqual(false, Kata.ValidatePin(".234"), "Wrong output for \".234\"");
+        }
+        
+        [Test, Description("ValidatePin should return true for valid pins")]
+        public void ValidTest()
+        {
+            Assert.AreEqual(true, Kata.ValidatePin("1234"), "Wrong output for \"1234\"");
+            Assert.AreEqual(true, Kata.ValidatePin("0000"), "Wrong output for \"0000\"");
+            Assert.AreEqual(true, Kata.ValidatePin("1111"), "Wrong output for \"1111\"");
+            Assert.AreEqual(true, Kata.ValidatePin("123456"), "Wrong output for \"123456\"");
+            Assert.AreEqual(true, Kata.ValidatePin("098765"), "Wrong output for \"098765\"");
+            Assert.AreEqual(true, Kata.ValidatePin("000000"), "Wrong output for \"000000\"");
+            Assert.AreEqual(true, Kata.ValidatePin("090909"), "Wrong output for \"090909\"");
+        }
+    }
+
+    [TestFixture]
+    public static class RevrotTests 
+    {
+        private static void testing(string actual, string expected) 
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public static void test1() 
+        {
+            Console.WriteLine("Testing RevRot");        
+            testing(Kata.RevRot("1234", 0), "");
+            testing(Kata.RevRot("", 0), "");
+            testing(Kata.RevRot("1234", 5), "");
+            String s = "733049910872815764";
+            testing(Kata.RevRot(s, 5), "330479108928157");
+        }
+    }     
+
+    [TestFixture]
+    public class IsPrimeTests
+    {
+        private static IEnumerable<TestCaseData> sampleTestCases
+        {
+            get
             {
-                var s = "green yellow red".Split()[rnd.Next(0, 3)];
-                var expected = SolutionUpdateLight(s);
-                Assert.That(Kata.UpdateLight(s), Is.EqualTo(expected));
+                yield return new TestCaseData(0).Returns(false);
+                yield return new TestCaseData(1).Returns(false);
+                yield return new TestCaseData(2).Returns(true);
             }
         }
-        readonly List<string> a = new List<string> { "green", "yellow", "red", "green" };
-        string SolutionUpdateLight(string current) => a[a.IndexOf(current) + 1];
-    }
 
-
-    [TestFixture]
-    public class GreetTest
-    {
-        [Test]
-        public void ShouldReturnHelloWorld()
-        {
-            Assert.AreEqual(Kata.Greet(), "hello world!");
-        }
-    }
-
-
-    [TestFixture]
-    public class GetMiddleTest
-    {
-        [Test]
-        public void GenericTests()
-        {
-            Assert.That(() => Kata.GetMiddle(null), Throws.ArgumentException, "didnt throw when input is null");
-            Assert.That(() => Kata.GetMiddle(""), Throws.ArgumentException, "didnt throw when input is empty");
-            Assert.AreEqual("es", Kata.GetMiddle("test"));
-            Assert.AreEqual("t", Kata.GetMiddle("testing"));
-            Assert.AreEqual("dd", Kata.GetMiddle("middle"));
-            Assert.AreEqual("A", Kata.GetMiddle("A"));
-        }
+        [Test, TestCaseSource("sampleTestCases")]
+        public bool SampleTest(int n) => Kata.IsPrime(n);
     }
 
     [TestFixture]
-    public class CheckIfFlush
+    public class BouncingBallTests 
     {
-         [Test]
-        public void BasicTests()
-        {
-            string[] cards={"AS", "3S", "9S", "KS", "4S"};
-            Assert.AreEqual(true, Kata.CheckIfFlush(cards));
-            string[] cards2={"AD", "4S", "7H", "KC", "5S"};
-            Assert.AreEqual(false, Kata.CheckIfFlush(cards2));
-                string[] cards3={"10D", "4S", "7H", "KC", "5S"};
-            Assert.AreEqual(false, Kata.CheckIfFlush(cards3));    
-                string[] cards4={"10D", "QD", "7D", "KD", "5D"};
-            Assert.AreEqual(true, Kata.CheckIfFlush(cards4));
-        }
-    } 
 
+        [Test]
+        public void Test1() {
+            Assert.AreEqual(3, Kata.bouncingBall(3.0, 0.66, 1.5));
+        }
+
+        [Test]
+        public void Test2() {
+            Assert.AreEqual(15, Kata.bouncingBall(30.0, 0.66, 1.5));
+        }
+    }
+    
     [TestFixture]
-    public class SortArrayTests
+    public class TimedReading
     {
         [Test]
-        public void BasicTests()
-        {
-            Assert.AreEqual(new int[] { 1, 3, 2, 8, 5, 4 }, Kata.SortArray(new int[] { 5, 3, 2, 8, 1, 4 }));
-            Assert.AreEqual(new int[] { 1, 3, 5, 8, 0 }, Kata.SortArray(new int[] { 5, 3, 1, 8, 0 }));
-            Assert.AreEqual(new int[] { }, Kata.SortArray(new int[] { }));
+        public void BasicTests(){
+            Assert.AreEqual(7,  Kata.TimedReading(4,"The Fox asked the stork, 'How is the soup?'"));
+            Assert.AreEqual(0,  Kata.TimedReading(1,"..."));
+            Assert.AreEqual(3,  Kata.TimedReading(3,"This play was good for us."));
+            Assert.AreEqual(5,  Kata.TimedReading(3,"Suddenly he stopped, and glanced up at the houses"));
+            Assert.AreEqual(11,  Kata.TimedReading(6,"Zebras evolved among the Old World horses within the last four million years."));
+            Assert.AreEqual(6,  Kata.TimedReading(5,"Although zebra species may have overlapping ranges, they do not interbreed."));
+            Assert.AreEqual(0,  Kata.TimedReading(1,"Oh!"));
+            Assert.AreEqual(14,  Kata.TimedReading(5,"Now and then, however, he is horribly thoughtless, and seems to take a real delight in giving me pain."));
         }
+    }
+
+    [TestFixture]
+    public class RgbToHex 
+    {
+        [Test]
+        public void FixedTests() 
+        {
+            Assert.AreEqual("FFFFFF", Kata.Rgb(255,255,255));
+            Assert.AreEqual("FFFFFF", Kata.Rgb(255,255,300));
+            Assert.AreEqual("000000", Kata.Rgb(0,0,0));
+            Assert.AreEqual("9400D3", Kata.Rgb(148,0,211));
+            Assert.AreEqual("9400D3", Kata.Rgb(148,-20,211), "Handle negative numbers.");
+            Assert.AreEqual("90C3D4", Kata.Rgb(144,195,212));
+            Assert.AreEqual("D4350C", Kata.Rgb(212,53,12), "Consider single hex digit numbers.");
+        }                
+    }
+
+    [TestFixture]
+    public class HumanReadableTime 
+    {
+        [Test]
+        public void HumanReadableTimeTest() 
+        {
+            Assert.AreEqual("00:00:00", Kata.GetReadableTime(0));
+            Assert.AreEqual("00:00:05", Kata.GetReadableTime(5));
+            Assert.AreEqual("00:01:00", Kata.GetReadableTime(60));
+            Assert.AreEqual("23:59:59", Kata.GetReadableTime(86399));
+            Assert.AreEqual("99:59:59", Kata.GetReadableTime(359999));
+        }                
     }
 }
--- Kata/Program.cs
+++ Kata/Program.cs
@@ -1,104 +1,195 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
 
 namespace Codewars
 {
     public class Kata
     {
-        #region UpdateLight
+        #region ValidatePin
 
         /// <summary>
-        /// https://www.codewars.com/kata/thinkful-logic-drills-traffic-light/solutions/csharp
+        /// https://www.codewars.com/kata/regex-validate-pin-code/train/csharp
         /// </summary>
-        public static string UpdateLight(string current)
+        public static bool ValidatePin(string pin)
         {
-            switch (current)
+            var pattern = @"^(\d{4})(\d{2})?$";
+            return Regex.Match(pin, pattern).Success;
+        } 
+
+        #endregion
+
+        #region Revrot
+
+        /// <summary>
+        /// https://www.codewars.com/kata/56b5afb4ed1f6d5fb0000991/train/csharp
+        /// </summary>
+        public static string RevRot(string strng, int sz)
+        {
+            if (sz <= 0 || sz > (strng?.Length ?? 0))
+                return string.Empty;
+
+            if (!Regex.IsMatch(strng, @"^[0-9]*$"))
+                throw new ArgumentException($"{nameof(strng)} must only contain numeric characters");
+
+            // Strip of extraneous chars that appear after chunk(s)
+            var str = strng.Substring(0, ((strng.Length / sz) * sz ));
+
+            var sb = new StringBuilder();
+            for (int i = 0; i < str.Length; i += sz)
             {
-                case "green":
-                    return "yellow";
-                case "yellow":
-                    return "red";
-                case "red":
-                    return "green";
-                default:
-                    throw new ArgumentException($"Unexpected {nameof(current)} value: {current}");
+                var chunk = str.Substring(i, sz);
+
+                // Calculate sum of qubes for digits in the chunck size
+                var sumOfQubes = chunk.ToCharArray().Select(s => Math.Pow(s % 10, 3)).Sum();
+
+                if ((sumOfQubes % 2) == 0)
+                {
+                    // Reverse
+                    sb.Append(new string(chunk.Reverse().ToArray()));
+                }
+                else
+                {
+                    // Rotate
+                    if (chunk.Length > 1)
+                    {
+                        sb.Append(chunk.Substring(1));
+                        sb.Append(chunk.Substring(0, 1));
+                    }
+                    else
+                        // jic sz == 1
+                        sb.Append(chunk);
+                }
             }
+        
+            return sb.ToString();
         }
 
         #endregion
 
-        #region Greet
+        #region IsPrime
 
         /// <summary>
-        /// https://www.codewars.com/kata/function-1-hello-world/solutions/csharp
+        /// https://www.codewars.com/kata/is-a-number-prime
         /// </summary>
-        public static string Greet() => "hello world!";
-
-        #endregion
-
-        #region GetMiddle
-
-        /// <summary>
-        /// https://www.codewars.com/kata/get-the-middle-character/solutions/csharp
-        /// </summary>
-        public static string GetMiddle(string s)
+        public static bool IsPrime(int n)
         {
-            if (String.IsNullOrEmpty(s))
-                throw new ArgumentException($"{nameof(s)} must not be null or empty string");
+            //https://stackoverflow.com/questions/1801391/what-is-the-best-algorithm-for-checking-if-a-number-is-prime
+            if (n <= 1)
+                return false;
+            if (n == 2 || n == 3)
+                return true;
+            if (n % 2 == 0)
+                return false;
+            if (n % 3 == 0)
+                return false;
 
-            var offset = (1 == s.Length % 2) ? 0 : 1;
-            return s.Substring((s.Length / 2) - offset, 1 + offset);
+            var i = 5;
+            var w = 2;
+
+            while (i * i <= n)
+            {
+                if (n % i == 0)
+                    return false;
+
+                i += w;
+                w = 6 - w;
+            }
+
+            return true;
         }
 
         #endregion
 
-        #region CheckIfFlush
+        #region Bouncing Balls
 
         /// <summary>
-        /// https://www.codewars.com/kata/determine-if-the-poker-hand-is-flush/train/csharp
+        /// https://www.codewars.com/kata/bouncing-balls
         /// </summary>
-        public static bool CheckIfFlush(string[] cards)
+        public static int bouncingBall(double h, double bounce, double window) 
         {
-            if ((cards?.Count() ?? 0) != 5)
-                throw new ArgumentException($"{nameof(cards)} must contain 5 elements");
+            // initial drop height must not be zero
+            if (h == 0)
+                return -1;
 
-            var suit = cards[0].Substring(cards[0].Length - 1);
-            return !cards.Any(a => !a.EndsWith(suit));
+            // bounce must have fractional value between zero and one
+            if (bounce <= 0 || bounce >= 1)
+                return -1;
+
+            // window must be below initial drop height
+            if (window >= h)
+                return -1;
+
+            return (int)(Math.Ceiling((Math.Log(window / h) / Math.Log(bounce))) * 2) - 1;
         }
 
         #endregion
 
-        #region SortArray
+        #region Timed Reading
 
         /// <summary>
-        /// https://www.codewars.com/kata/sort-the-odd/train/csharp
+        /// https://www.codewars.com/kata/simple-fun-number-40-timed-reading
         /// </summary>
-        public static int[] SortArray(int[] array)
+        public static int TimedReading(int MaxLength, string text) 
         {
-            if (array == null)
-                throw new ArgumentException($"{nameof(array)} must not be null");
+            return Regex.Replace(text, @"\p{P}", "")
+                .Split(" ")
+                .Where(w => w.Length > 0 && w.Length <= MaxLength)
+                .Count();
+        }
 
-            var oddsSorted = array.Where(w => (w % 2) != 0).OrderBy(o => o);
+        #endregion
+
+        #region RGB To Hex Conversion
+
+        /// <summary>
+        /// https://www.codewars.com/kata/rgb-to-hex-conversion/train/csharp
+        /// </summary>
+        public static string Rgb(int r, int g, int b) 
+        {
+            byte[] ba = new byte[3];
             var idx = 0;
-            return array.Select(s => 
-            { 
-                    if ((s % 2) != 0)
-                        return oddsSorted.ElementAt(idx++);
-                    return s;
-            }).ToArray();
+            foreach(var i in new int[] {r, g, b})
+            {
+                b = i < 0 ? 0 : i; 
+                b = b > 255 ? 255 : b;
+                ba[idx++] = (byte)b;
+            }
+
+            return $"{String.Concat(ba.Select(e =>e.ToString("X2")))}";
         }
 
         #endregion
 
+        #region Human Readable Time
+
+        /// <summary>
+        /// https://www.codewars.com/kata/52685f7382004e774f0001f7/train/csharp
+        /// </summary>
+        public static string GetReadableTime(int seconds) 
+        {
+            if (0 > seconds || seconds > 359999 )
+                throw new ArgumentException($"{nameof(seconds)} must be greater than or equal 0 and less than or equal to 359999");
+
+            int hh = seconds / (60 * 60);
+            int mm = (seconds - (hh * 60 * 60)) / 60;
+            int ss = seconds - (hh * 60 * 60) - (mm * 60 * 60) / 60;
+
+            return $"{hh:D2}:{mm:D2}:{ss:D2}";
+        }
+
+        #endregion
 
         static void Main(string[] args)
         {
-            //Console.WriteLine($"green -> {UpdateLight("green")}");
-            //Console.WriteLine($"{greet()}");
-            //Console.WriteLine($"{GetMiddle("test")}");
-            //Console.WriteLine($"{Kata.CheckIfFlush(new string[] {"AS", "3S", "9S", "KS", "4S"})}");
-            Console.WriteLine(String.Join(",", Kata.SortArray(new int[] {5, 3, 2, 8, 1, 4}).Select(s => s.ToString())));
-
+            //Console.WriteLine($"{ValidatePin("92495")}");
+            //Console.WriteLine($"{RevRot("9249590956065", 4)}");
+            //Console.WriteLine($"{bouncingBall(109, 0.75, 1.09)}");
+            //Console.WriteLine($"{TimedReading(1,"...")}");
+            //Console.WriteLine($"{Rgb(257,-10,254)}");
+            Console.WriteLine($"{GetReadableTime(359999)}");
+            //Console.WriteLine($"{GetReadableTime(5)}");
             //Console.ReadKey();
         }
     }
